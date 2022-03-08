b57d6298698e888610a63437a579fb88cc55b823
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-07 23:21:17 -0300

    feat: add global margins

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,9 @@ import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className='max-w-screen-lg mx-auto px-2'>
+      <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 )
