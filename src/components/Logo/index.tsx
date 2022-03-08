88d65952518b71a7c242f2f0386cfcbf438aabb2
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-07 23:18:19 -0300

    feat: create logo

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'wouter'
+
+const Logo: React.FC = () => {
+  return (
+    <Link href='/' className='text-xl'>
+      Fake<span className='font-bold'>Store</span>
+    </Link>
+  )
+}
+
+export default Logo
