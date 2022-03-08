e3363b44f7e6efc24d4946d0ad75811e5591e67d
Mateo Álvarez <alvarez.fing@gmail.com>
2022-03-07 23:21:01 -0300

    feat: add types of products

---
+++ src/types/productsType.ts
@@ -0,0 +1,18 @@
+interface IRating {
+  rate: number
+  count: number
+}
+
+export interface IProduct {
+  id: number
+  title: string
+  price: number
+  description: string
+  category: string
+  image: string
+  rating: IRating
+}
+
+export interface IProducts {
+  products: IProduct[]
+}
